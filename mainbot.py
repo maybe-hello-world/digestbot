51c783ed437405117ff77f391a9a4ff35a7cb42f
maybe-hello-world <maybe.hello.world@gmail.com>
2019-10-01 17:49:00 +0300

    working now

--- mainbot.py
+++ mainbot.py
@@ -4,6 +4,4 @@ import slack
 client = slack.WebClient(token=os.environ['SLACK_API_TOKEN'])
 
 channels = client.channels_list()
-
-for i in channels:
-    print(client.conversations_history(i))
+print(channels)
