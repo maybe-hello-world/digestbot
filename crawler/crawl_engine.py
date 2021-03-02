3145036d8a4042abf28b599888522cb9a0455cd4
julshac <julia7476@gmail.com>
2021-03-02 21:37:17 +0300

    Modify crawler: replace requests with try_requests

--- crawler/crawl_engine.py
+++ crawler/crawl_engine.py
@@ -4,6 +4,7 @@ import json
 from logging import Logger
 import requests as r
 
+from common.extras import try_request
 from common.models import Message
 from common import Slacker
 import crawler.config as config
@@ -23,27 +24,23 @@ async def crawl_messages_once(
             prev_date = datetime.now() - timedelta(days=config.MESSAGE_DELTA_DAYS)
             messages = await slacker.get_channel_messages(ch_id, prev_date)
             if messages:
-                answer = r.put(base_url + "message", data=json.dumps(messages), timeout=10)
-                if answer.status_code != 200:
-                    logger.error(answer.text)
-            logger.debug(len(messages))
+                try_request(r.put, base_url + "message", data=json.dumps(messages))
+
         logger.info(
             f"Messages from {len(ch_info)} channels parsed and sent to the database."
         )
 
     # update messages without permalinks
-    answer = r.get(base_url + "message/linkless", timeout=10)
-    if answer.status_code != 200:
-        logger.error(answer.text)
+    answer = try_request(r.get, base_url + "message/linkless")
+    if answer.is_err():
         return
+    answer = answer.value
 
     empty_links_messages = [Message(**x) for x in answer.json()]
     if empty_links_messages:
         messages = await slacker.update_permalinks(messages=empty_links_messages)
-        answer = r.patch(base_url + "message/links", data=json.dumps(messages), timeout=10)
-        if answer.status_code != 200:
-            logger.error(answer.text)
-        else:
+        answer = try_request(r.patch, base_url + "message/links", data=json.dumps(messages))
+        if answer.is_ok():
             logger.debug(f"Updated permalinks for {len(messages)} messages.")
 
 
