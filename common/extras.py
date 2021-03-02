f8d9f64dc24e2e37622f4e804883b1045aee1e76
julshac <julia7476@gmail.com>
2021-03-02 21:36:32 +0300

    Introduce try_request for generic requests processing

--- common/extras.py
+++ common/extras.py
@@ -1,6 +1,9 @@
 import json
 from datetime import datetime, timedelta
-from typing import Optional
+from typing import Optional, Callable
+import result
+import requests as r
+from sentry_sdk import capture_exception
 
 
 def try_parse_int(value: str) -> Optional[int]:
@@ -17,3 +20,15 @@ class TimerEncoder(json.JSONEncoder):
         if isinstance(o, timedelta):
             return o.total_seconds()
         return json.JSONEncoder.default(self, o)
+
+
+def try_request(request: Callable, *args, **kwargs) -> result.Result[r.Response, str]:
+    try:
+        answer: r.Response = request(*args, **kwargs, timeout=10)
+        if answer.status_code != 200:
+            raise ValueError(answer.text)
+
+        return result.Ok(answer)
+    except (r.exceptions.Timeout, ValueError) as e:
+        capture_exception(e)
+        return result.Err(str(e))
