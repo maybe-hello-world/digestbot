77161504d098fd9f5027412080d5fc20485076fd
maybe-hello-world <maybe.hello.world@gmail.com>
2021-03-06 19:18:43 +0300

    Help updated

--- uiservice/routers/helper.py
+++ uiservice/routers/helper.py
@@ -2,14 +2,8 @@ import container
 
 
 def general_help() -> str:
-    return (
-        "Hi! I'm a bot who can send you a top messages from given channels.\n"
-        "For asking help for any command type: `help command_name`\n\n"
-        "Available commands:\n"
-        "`top`\n"
-        "`presets`\n"
-        "`timers`"
-    )
+    template = container.jinja_env.get_template("help_response.json")
+    return template.render()
 
 
 def unrecognized_help() -> str:
@@ -23,29 +17,49 @@ def presets_help() -> str:
         "There are two types of presets: global and local:\n"
         "* global - predefined presets (by bot authors, contributions are welcome :) )\n"
         "* local - presets that you can create\n\n"
-        "With help of presets you can union channels in categories that may interest you\n"
+        "With help of presets you can union channels in categories that may interest you and then specify preset "
+        "instead of subset of channels for simplicity."
     )
 
 
 def timers_help() -> str:
-    return "Allows you to add, remove or list timers which send you top results on schedule."
+    return (
+        "Allows you to add, remove or list timers.\n"
+        "These timers could be created for providing you top messages on schedule, like every day or week.\n"
+        "You can configure different settings and specify channels or presets from which to provide messages.\n"
+        "It's not so hard, so we encourage you to try :)"
+    )
 
 
 def top_help() -> str:
-    return "Returns top messages from requested channels."
+    return (
+        "Returns top messages from requested channels. \n"
+        "Top is defined by selecting messages with given criteria and sorting them by one of the available methods.\n"
+        "Just ask `top` and try it by yourself :)."
+    )
+
+
+def qna_help() -> str:
+    return (
+        "Q&A ODS.ai team provides an API for intellectual search over ODS.ai Slack workspace messages. \n"
+        "Via this bot you can use this API and benefit from modern NLP approaches working just for you ^^. \n"
+        "You can contact the Q&A ODS.ai team via "
+    )
 
 
 async def process_message(channel_id: str, text: str):
-    help_answer = unrecognized_help()
-    text = text.strip()
+    help_answer = {"text": unrecognized_help()}
+    text = text.strip().replace("+", " ")
 
     if text == "help":
-        help_answer = general_help()
+        help_answer = {"blocks": general_help()}
     elif text == "help presets":
-        help_answer = presets_help()
+        help_answer = {"text": presets_help()}
     elif text == "help top":
-        help_answer = top_help()
+        help_answer = {"text": top_help()}
     elif text == "help timers":
-        help_answer = timers_help()
+        help_answer = {"text": timers_help()}
+    elif text == "help qna":
+        help_answer = {"text": qna_help()}
 
-    await container.slacker.post_to_channel(channel_id=channel_id, text=help_answer)
+    await container.slacker.post_to_channel(channel_id=channel_id, **help_answer)
